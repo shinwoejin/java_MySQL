cd4141d24634ea8cf1ea47a2aba406af3c54006e
shinwoejin <shinwoejin@naver.com>
2022-09-05 09:42:38 +0900

    commit

--- src/MiniProjectConnectMysql.java
+++ src/MiniProjectConnectMysql.java
@@ -5,6 +5,8 @@ public class MiniProjectConnectMysql {
         // Connection Method, hostname, port, username을 맞춘후
         // 들어갈 Default Schema 를 작성후 Connection
         // edit창 띄우기
+        // mysql 명령어 입력
+        // 실행후 조회
         return ;
     }
 }
