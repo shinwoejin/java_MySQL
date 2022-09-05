9a377b198ed239f31e44487ac6fddcc11f30a80f
shinwoejin <shinwoejin@naver.com>
2022-09-05 11:25:49 +0900

    commit

--- src/MiniProjectConnectionMysql_me.java
+++ src/MiniProjectConnectionMysql_me.java
@@ -4,7 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MiniProjectConnectMysql {
+public class MiniProjectConnectionMysql_me {
+    
     public static void main(String[] args) {
         // Mysql 열기 -> mysql-connector J
         // Connect to database 클릭
@@ -15,10 +16,9 @@ public class MiniProjectConnectMysql {
         final String USER = "root";
         final String PASS = "tbrs00002b";
         final String DB_URL = "jdbc:mysql://localhost/world";
-        String QUERY = "select ID, Name, Population "+
-                        "from city " +
-                        "where 1 = 1 " +
-                        "and Population >= 9230000";
+        String QUERY = "select Code, Name, Continent, Region " +
+                        "from country " +
+                        "where Name like ('an%')";
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement(); // sql파일 만드는 구문
@@ -28,9 +28,10 @@ public class MiniProjectConnectMysql {
         // 갯수를 모를땐 while 안다면 for문을 사용
             while (rs.next()) {
                 // Retrieve by column name
-                System.out.print("ID: " + rs.getInt("ID"));
+                System.out.print("Code: " + rs.getString("Code"));
                 System.out.print(", Name: " + rs.getString("Name"));
-                System.out.print(", Population: " + rs.getInt("Population"));
+                System.out.print(", Continent: " + rs.getString("Continent"));
+                System.out.print(", Region: " + rs.getString("Region"));
             }
             
         } catch (SQLException e) {
