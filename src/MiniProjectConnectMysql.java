31026ad893c7eedf8048fb4ce0c0aae158bfb850
shinwoejin <shinwoejin@naver.com>
2022-09-05 09:39:23 +0900

    com

--- src/MiniProjectConnectMysql.java
+++ src/MiniProjectConnectMysql.java
@@ -1,5 +1,10 @@
 public class MiniProjectConnectMysql {
     public static void main(String[] args) {
+        // MysqlWorkbench
+        // Connect to database
+        // Connection Method, hostname, port, username을 맞춘후
+        // 들어갈 Default Schema 를 작성후 Connection
+        // edit창 띄우기
         return ;
     }
 }
