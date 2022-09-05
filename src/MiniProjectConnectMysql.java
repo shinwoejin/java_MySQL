bff9b59e121d9a9d2111d8a746b73cabf6fc565d
shinwoejin <shinwoejin@naver.com>
2022-09-05 10:25:15 +0900

    com

--- src/MiniProjectConnectMysql.java
+++ src/MiniProjectConnectMysql.java
@@ -7,10 +7,11 @@ public class MiniProjectConnectMysql {
         // Connect to database 클릭
         // Connection Method, hostname, port, username을 맞춘후
         // 들어갈 Default Schema 를 작성후 Connection
-        // Hostname : localhost, port : 3306, Username : root
-        // default schema : world, passworld : tbrs00002b
+        // Hostname : localhost, port : 3306 
+        // Username : root, default schema : world, passworld : tbrs00002b
         final String USER = "root";
         final String PASS = "tbrs00002b";
+        final String DB_URL = "jdbc:mysql://localhost/world";
         Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         // access to 'World' DB, edit창 띄우기
         // mysql 명령어 입력후,  실행후 조회(결과 확인)
