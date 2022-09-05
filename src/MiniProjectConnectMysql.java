e146bf596045ad0b42900069366959d4a79ec988
shinwoejin <shinwoejin@naver.com>
2022-09-05 13:54:10 +0900

    commit

--- src/MiniProjectConnectMysql.java
+++ src/MiniProjectConnectMysql.java
@@ -22,7 +22,15 @@ public class MiniProjectConnectMysql {
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement(); // sql파일 만드는 구문
-            // 새로운 query생성, select 명령문 입력 후 test 해보기
+            // insert
+            // int id = 323214;
+            QUERY = "insert into city (ID, Name, CountryCode, District, Population) " +
+                    "values (98765, 'Name', 'afg', 'District', 500)";
+            int value = stmt.executeUpdate(QUERY);
+            // select
+            QUERY = "select ID, Name, CountryCode, District, Population " +
+                    "from city " +
+                    "where Name = 'Name'";
             ResultSet rs = stmt.executeQuery(QUERY); // 결과 값이 담기는 집합
             // result grid, action output 확인
             // 갯수를 모를땐 while 안다면 for문을 사용
@@ -30,8 +38,24 @@ public class MiniProjectConnectMysql {
                 // Retrieve by column name
                 System.out.print("ID: " + rs.getInt("ID"));
                 System.out.print(", Name: " + rs.getString("Name"));
+                System.out.print(", CountryCode: " + rs.getString("CountryCode"));
+                System.out.print(", District: " + rs.getString("District"));
                 System.out.print(", Population: " + rs.getInt("Population"));
             }
+            // update
+            QUERY = "update city " +
+                    "set Population = 2000 " +
+                    "where Name = 'Name' ";
+            value = stmt.executeUpdate(QUERY);
+            // select
+            System.out.println();
+            // delete
+            QUERY = "delete from city "+
+            "where Name = 'Name' ";
+            value = stmt.executeUpdate(QUERY);
+            // select
+            System.out.println();
+            // 새로운 query생성, select 명령문 입력 후 test 해보기
 
         } catch (SQLException e) {
             e.printStackTrace();
